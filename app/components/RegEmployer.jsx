6ea7ac1dc54e47e1c75e4eecdd6c25055f5b8eb6
User <user@Users-MacBook-Pro.local>
2016-09-20 23:23:22 -0400

    Added Register Pages

---
+++ app/components/RegEmployer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var RegEmployer = React.createClass({
+  render: function(){
+    return(
+      <h2>Register Account - Employer</h2>
+    );
+  }
+});
+
+module.exports = RegEmployer;
