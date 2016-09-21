854c5d05f2b2127d0270480623c42b999c687f53
User <user@Users-MacBook-Pro.local>
2016-09-20 23:23:36 -0400

    Added Register Pages

---
+++ app/components/RegSeeker.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var RegSeeker = React.createClass({
+  render: function(){
+    return(
+      <h2>Register Account - Job Seeker</h2>
+    );
+  }
+});
+
+module.exports = RegSeeker;
