febbd633c7e3ef37be48f7bea45c32475f0be6b6
User <user@Users-MacBook-Pro.local>
2016-09-20 22:28:51 -0400

    Added Inputs to page

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -3,7 +3,18 @@ var React = require('react');
 var Login = React.createClass({
   render: function(){
     return(
+<div>
       <h2>Login</h2>
+<div>
+    <label>User Name:</label>
+    <input type="text" name="username"/>
+</div>
+<div>
+    <label>Password</label>
+    <input type="text" name="password"/>
+</div>
+
+</div>
     );
   }
 });
