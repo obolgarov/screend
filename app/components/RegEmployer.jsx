885444dfa72e4e03f48b39d6a03a5f7e35eb749b
User <user@Users-MacBook-Pro.local>
2016-09-23 13:32:22 -0400

    onclick

--- app/components/RegEmployer.jsx
+++ app/components/RegEmployer.jsx
@@ -1,65 +1,82 @@
 var React = require('react');
 
 var RegEmployer = React.createClass({
+
+    onSubmit: function (e) {
+      var data = {
+        firstName : this.refs.first.getDOMNode.value,
+        lastName : this.refs.last.getDOMNode.value,
+        userEmail : this.refs.email.getDOMNode.value,
+        userName : this.refs.user.getDOMNode.value,
+        userPass : this.refs.password.getDOMNode.value,
+        comName : this.refs.companyName.getDOMNode.value,
+        comAddress : this.refs.companyAddress.getDOMNode.value,
+        comIndustry : this.refs.industry.getDOMNode.value,
+        comAttributes : this.refs.comAttributes.getDOMNode.value
+         }
+    },
+
   render: function(){
     return(
 
   <div>
       <h2>Register Account - Employer</h2>
 
+    <form ref='employer_form' onSubmit={this.onSubmit}>
         <div>
             <label>First Name: </label>
-            <input type="text" name="first"/>
+            <input type="text" ref="first"/>
         </div>
 
         <div>
             <label>Last Name: </label>
-            <input type="text" name="last"/>
+            <input type="text" ref="last"/>
         </div>
         <div>
             <label>Email: </label>
-            <input type="email" name="email"/>
+            <input type="email" ref="email"/>
         </div>
 
         <div>
             <label>Preferred Username: </label>
-            <input type="text" name="user"/>
+            <input type="text" ref="user"/>
         </div>
 
         <div>
             <label>Password: </label>
-            <input type="password" name="password"/>
+            <input type="password" ref="password"/>
         </div>
 
         <div>
             <label>Confirm Password: </label>
-            <input type="password" name="confirmPassword"/>
+            <input type="password" ref="confirmPassword"/>
         </div>
 
         <div>
             <label>Company Name: </label>
-            <input type="text" name="companyName"/>
+            <input type="text" ref="companyName"/>
         </div>
 
         <div>
             <label>Company Address: </label>
-            <input type="text" name="companyAddress"/>
+            <input type="text" ref="companyAddress"/>
         </div>
 
         <div>
             <label>Industry: </label>
-            <input type="text" name="industry"/>
+            <input type="text" ref="industry"/>
         </div>
 
         <div>
             <label>Attributes: </label>
-        <textarea name="attributes"></textarea>
+        <textarea ref="attributes"></textarea>
         </div>
 
         <div>
           <button type="submit">Submit</button>
         </div>
 
+      </form>
   </div>
   );
   }
