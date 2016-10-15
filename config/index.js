e4fde8b7808a261fefa5c6891947fd20dc558d80
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2016-10-15 17:40:53 -0400

    continued support for jwt

--- config/index.js
+++ config/index.js
@@ -2,17 +2,20 @@ var config = {
   local: {
     mode: 'local',
     port: 3000,
-    connstr: 'mongodb://localhost:27017/screend'
+    connstr: 'mongodb://localhost:27017/screend',
+    secret: 'secret'
   },
   staging: {
     mode: 'staging',
     port: 4000,
-    connstr: 'mongodb://localhost:27017/screend'
+    connstr: 'mongodb://localhost:27017/screend',
+    secret: 'secret'
   },
   production: {
     mode: 'production',
     port: 5000,
-    connstr: 'mongodb://localhost:27017/screend'
+    connstr: 'mongodb://localhost:27017/screend',
+    secret: 'secret'
   }
 }
 
