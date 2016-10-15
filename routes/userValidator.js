678654d534b3dd8f5eb696fe08383ac88a4e5de7
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2016-10-15 17:43:59 -0400

    continued support for jwt

--- routes/userValidator.js
+++ routes/userValidator.js
@@ -1,13 +1,32 @@
+var jwt = require('jsonwebtoken');
+var config = require('../config')();
 
 
 var userValidator = {
 
-  validate: function () {
+  validateUser: function () {
 
     //TODO: validate
 
     return true;
 
+  },
+
+  validateToken: function(token, callback) {
+
+    jwt.verify(token, config.secret, callback); // passes the decoded token body into callback
+
+  },
+
+  generateToken: function(payload, callback) {
+
+
+    var options = {
+      expiresIn : Math.floor(new Date().getTime()/1000) + 24*60*60, // current unix timestamp + 24 hours
+      algorithm : "HS256"
+    }
+
+    var token = jwt.sign(payload, config.secret, options, callback);
   }
 
 }
