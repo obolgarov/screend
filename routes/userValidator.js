9cf5a73ef8857b67f3cd4ba2146d9e3425d949d0
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2016-10-21 12:50:04 -0400

    user validation file

--- routes/userValidator.js
+++ routes/userValidator.js
@@ -1,11 +1,32 @@
+var jwt = require('jsonwebtoken');
+var config = require('../config')();
 
 
 var userValidator = {
 
-  validate : function(token) {
+  validateUser: function () {
 
-    //TODO: validate here
+    //TODO: validate
 
+    return true;
+
+  },
+
+  validateToken: function(token, callback) {
+
+    jwt.verify(token, config.secret, callback); // passes the decoded token body into callback
+
+  },
+
+  generateToken: function(payload, callback) {
+
+
+    var options = {
+      expiresIn : Math.floor(new Date().getTime()/1000) + 24*60*60, // current unix timestamp + 24 hours
+      algorithm : "HS256"
+    }
+
+    var token = jwt.sign(payload, config.secret, options, callback);
   }
 
 }
