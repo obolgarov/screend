3ec7541291872ef5456ceab003d38c48ba85a4c7
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2016-10-30 16:48:25 -0400

    dynamic skills

--- app/components/PostJobForm.jsx
+++ app/components/PostJobForm.jsx
@@ -5,17 +5,27 @@ var querystring = require('querystring'); // to send data inside the request
 var {Link} = require('react-router');
 var Nav = require('Nav');
 
-var postJobForm = React.createClass({
+
+// "That's the thing with random numbers, you can never be sure."
+var randomNum = 0;
+var generateRandomID = function(){
+  var nextRandomNum = randomNum + 1;
+  randomNum = nextRandomNum;
+  return randomNum;
+}
+
+var PostJobForm = React.createClass({
 
   getInitialState: function() {
     var initialData = [];
     initialData.push({
-      iteration: 0,
+      id: generateRandomID(),
       skill: "",
-      locked: false
-    })
+      locked: false,
+    });
     return {
-      data: initialData
+      data: initialData,
+
     };
   },
 
@@ -45,7 +55,7 @@ var postJobForm = React.createClass({
         'Accept': 'application/json'
       },
       body: dataQuerystring
-    }
+    };
 
     console.log("body: " + JSON.stringify(data));
 
@@ -84,38 +94,126 @@ var postJobForm = React.createClass({
   },
 
   // deletes selected skill if it's recorded, adds new skill if it's a new field
-  deleteSkill: function(prevIteration, prevSkill) {
+  deleteSkill: function(fieldID) {
 
     var currentState = this.state;
 
-    // validate the reference's field
-    // TODO
+    // find skill object in array
+    for ( var skill of currentState.data ) {
+      if(skill.id == fieldID){
 
-    var prevSkill = currentState.data[prevIteration];
+        // use exact skill object to get index in array
+        var skillIndex = currentState.data.indexOf(skill);
 
-    //currentState.data[prevIteration].skill =
+        // use index to remove skill from array
+        currentState.data.splice(skillIndex, 1);
 
-    // add skill
-    currentState.data.push({
-      iteration: prevIteration + 1
-    });
+        break;
+      }
+    }
+
+    // do nothing with current state if skill id not found
+
+    // replace state with new state, updating list
+    this.setState(currentState);
+
+  },
+
+  updateSkillState: function(fieldID){
+
+    var currentState = this.state;
+
+
+    // find skill object in array
+    for ( var skill of currentState.data ) {
+      if(skill.id == fieldID){
+
+        console.log("test");
+
+        //TODO: check if skill valid
+
+
+        var skillIndex = currentState.data.indexOf(skill);
+
+        currentState.data[skillIndex].locked = true;
+
+        break;
+      }
+    }
+    // do nothing with current state if skill id not found
+
+
+    // replace state with new state, updating list
+    this.setState(currentState);
+
+  },
+
+  updateSkillText: function(fieldID, event){
+    var text = event.target.value;
+    var currentState = this.state;
+
+    for ( var skill of currentState.data ) {
+      if(skill.id == fieldID){
+
+        var skillIndex = currentState.data.indexOf(skill);
+
+        currentState.data[skillIndex].skill = text;
+
+        break;
+      }
+    }
+
     this.setState(currentState);
 
   },
 
   renderSkillList: function() {
     if (this.state.data){
-      var skillList = this.state.data.map((result) => {
-        var key = "key_" + result.iteration; // react seems to want something as a unique key, works without it but playing it safe
+      var skillList = this.state.data.map((skillData) => {
+        var key = "key_" + skillData.id; // react seems to want something as a unique key, works without it but playing it safe
+
+
+        //because jsx code doesn't allow conditionals, they're written here instead
+
+        var lockedStyle = {
+          background: "white",
+          display: "inline-block"
+        };
+        var deleteButton = null;
+        if (skillData.locked){
+          lockedStyle = {
+            background: "grey",
+            display: "inline-block"
+          };
+          deleteButton = (
+            <input type="button" ref="button" onClick={this.deleteSkill.bind(this, skillData.id)} value="x"/>
+          );
+        }
+
+        //console.log(deleteButton)
+
         return (
-          <div className="skillEntry" key={key}>
-            <input type="button" ref="button" onClick={deleteSkill(result.iteration)} value="x"/>
-            <FormInput value={result.skill}/>
+          <div className="skillEntry" key={key} style={{display: "block"}}>
+            {deleteButton}
+            <input type="text" value={skillData.skill} style={lockedStyle} onBlur={this.updateSkillState.bind(this, skillData.id)} onChange={this.updateSkillText.bind(this, skillData.id)}/>
           </div>
         );
       });
       return skillList;
     }
+  },
+
+  addSkillField: function() {
+    var currentState = this.state;
+
+    currentState.data.push({
+      id: generateRandomID(),
+      skill: "",
+      locked: false,
+    });
+
+    // replace state with new state, updating list
+    this.setState(currentState);
   },
 
   render: function() {
@@ -178,6 +276,7 @@ var postJobForm = React.createClass({
             {
               this.renderSkillList()
             }
+            <input type="button" ref="button" onClick={this.addSkillField} value="+"/>
           </div>
 
           <div>
@@ -189,4 +288,4 @@ var postJobForm = React.createClass({
   }
 });
 
-module.exports = postJobForm;
+module.exports = PostJobForm;
