92ef28cf62488cb759443176a40e75fb2a7830bb
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2016-11-10 11:46:34 -0500

    profile creation

--- app/components/CreateProfile.jsx
+++ app/components/CreateProfile.jsx
@@ -29,14 +29,9 @@ var generateRandomID = function(){
 var CreateProfile = React.createClass({
   componentDidMount: function() {
 
-<<<<<<< HEAD
     var myCookie = cookie.load('userToken');
     //console.log(myCookie);
 
-=======
- var myCookie = cookie.load('userToken');
- console.log(myCookie);
->>>>>>> 0a9b5f069d1b1559e981cbe3a02c44c7e0fc36c9
     if (myCookie == null)
     {
       hashHistory.push('Welcome');
@@ -509,7 +504,7 @@ var CreateProfile = React.createClass({
 
         <div ref="uploadForm" id="uploadFormId" className="columns medium-4 large-6 small-centered">
           <h2>Create Profile</h2>
-          
+
           <form ref="resume" encType="multipart/form-data" onSubmit={this.onSubmit}>
             <input type="file" name="resume" ref="resumeupload"></input>
             <input type="submit" value="UploadResume" ref="resumesubmit" name="submit" className="button hollow" style={button}></input>
