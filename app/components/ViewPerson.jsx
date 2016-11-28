027e6de1966eafb2728c05a1dd1cdd6533cc5a8c
Chris Goguen <Chris Goguen>
2016-11-27 20:16:26 -0500

    Blue Bar

--- app/components/ViewPerson.jsx
+++ app/components/ViewPerson.jsx
@@ -4,93 +4,98 @@ var httpGen = require('./httpGen.js');
 
 var ViewPerson = React.createClass({
 
-  componentDidMount() {
-    function getParameterByName(name, url) {
+    componentDidMount() {
+        function getParameterByName(name, url) {
 
-      if (!url) {
-        url = window.location.href;
-      }
-      name = name.replace(/[\[\]]/g, "\\$&");
-      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
+            if (!url) {
+                url = window.location.href;
+            }
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
 
-    var profileId = getParameterByName('id');
+        var profileId = getParameterByName('id');
 
-    var profileData = {
-      id : profileId
-    }
+        var profileData = {
+            id: profileId
+        }
 
- httpGen.generate({
+        httpGen.generate({
             data: profileData,
             path: "/profile/findProfile",
             method: "POST",
             onData: (data) => {
 
-          var profile = JSON.parse(data);
+                var profile = JSON.parse(data);
 
-          console.log(profile[0].owner)
- 
- 
-    var applicantData = {
-      user : profile[0].owner
+                console.log(profile[0].owner)
+
+
+                var applicantData = {
+                    user: profile[0].owner
+                }
+
+                httpGen.generate({
+                    data: applicantData,
+                    path: "/applicants/find",
+                    method: "POST",
+                    onData: (data) => {
+
+                        var applicant = JSON.parse(data);
+
+                        var name = document.createTextNode(applicant[0].firstname + " " + applicant[0].lastname);
+                        document.getElementById("name").appendChild(name);
+                        document.getElementById("name").appendChild(document.createElement("br"));
+
+                        var email = document.createTextNode(applicant[0].email);
+                        document.getElementById("email").appendChild(email);
+                        document.getElementById("email").appendChild(document.createElement("br"));
+                    }
+                },
+
+                )
+            }
+        })
+
+
+
+
+    },
+
+
+    render: function() {
+        return (
+            <div>
+                <Nav />
+
+                <div className="callout large primary">
+                    <div className="row column text-center">
+                        <h1>Contact Information</h1>
+                    </div>
+                </div>
+                <div>
+                    <h4> Name</h4>
+
+                    <label id="name"></label>
+
+                </div>
+
+                <br />
+                <div>
+                    <h4> Email</h4>
+
+                    <label id="email"></label>
+
+
+                </div>
+
+            </div>
+        );
     }
-
-    httpGen.generate({
-            data: applicantData,
-            path: "/applicants/find",
-            method: "POST",
-            onData: (data) => {
-
-          var applicant = JSON.parse(data);
-
-          var name = document.createTextNode(applicant[0].firstname + " " + applicant[0].lastname);
-          document.getElementById("name").appendChild(name);
-          document.getElementById("name").appendChild(document.createElement("br"));
-
-          var email = document.createTextNode(applicant[0].email);
-          document.getElementById("email").appendChild(email);
-          document.getElementById("email").appendChild(document.createElement("br"));
-    }},
-   
-    )}
- })
- 
-     
-        
-
-  },
-
-
-  render: function () {
-    return (
-      <div>
-        <Nav />
-        <h1>Contact Information</h1>
-
-        <div>
-          <h4> Name</h4>
-
-          <label id="name"></label>
-
-        </div>
-
-<br/>
-        <div>
-          <h4> Email</h4>
-
-          <label id="email"></label>
-
-        </div>
-
-
-
-      </div>
-    );
-  }
 });
 
 module.exports = ViewPerson;
